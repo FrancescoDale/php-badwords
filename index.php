e99772d1b2272a0393a69180e732e14cf4858247
FrancescoDale <fd060862@gmail.com>
2020-12-14 15:04:43 +0100

    assegnazione della stringa alla variabile $paragraph

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+$paragraph = "SpaceX inaugurò la produzione di veicoli di lancio con il Falcon 1, il quale venne progettato e fabbricato tra il 2008 e il 2009.È un razzo orbitale a due stadi che usa ossigeno liquido e RP-1 per entrambi gli stadi: il primo è spinto da un unico motore di classe Merlin e il secondo da un unico razzo di classe Kestrel. Il veicolo è stato lanciato per cinque volte, riuscendo a raggiungere l'orbita durante il quarto tentativo, nel settembre 2008, con della zavorra al posto del carico utile.In sostituzione del Falcon 1 venne pianificato anche un lanciatore denominato Falcon 5, ma il suo sviluppo è stato interrotto in favore dei Falcon 9.";
+
+?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title>Php Badwords</title>
+    </head>
+    <body>
+
+    </body>
+</html>
